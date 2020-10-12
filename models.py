b8634449c1e322240439de28f4a2fa5ddf439b6d
EC2 Default User <ec2-user@ip-172-31-49-111.ec2.internal>
2020-10-12 18:53:20 +0000

    Wrote Texts model and User model

--- models.py
+++ models.py
@@ -1,15 +1,21 @@
 # models.py
 import flask_sqlalchemy
 from app import db
+from datetime import datetime
 
 
-class Usps(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    address = db.Column(db.String(120))
+    name = db.Column(db.String(30), nullable=False)
+
+class Texts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.Text)
+    date = db.Column(db.DateTime, default=datetime.now)
+    user = db.Column(db.String(30), db.ForeignKey('user.id'))
     
-    def __init__(self, a):
-        self.address = a
+    def __init__(self, text):
+        self.text = text
         
     def __repr__(self):
-        return '<Usps address: %s>' % self.address 
-
+        return "<Text: {}\nBy: {}".format(self.text, self.user)
