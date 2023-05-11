a9a1159b1b00c458ef38a62bfc0f50bea7e8bfd0
Cristian Teran <cristianteranv@gmail.com>
2023-05-10 21:58:59 -0700

    trying with different initialization and url

--- scripts/Socket.jsx
+++ scripts/Socket.jsx
@@ -1,3 +1,3 @@
-import * as SocketIO from 'socket.io-client'
+import io from 'socket.io-client';
 
-export var Socket = SocketIO.connect('http://0.0.0.0:8080')
+export const Socket = io('http://localhost:8080')
